250b6d2a0cb737aa11094e2b5af0ea49c81452c1
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2025-03-05 22:03:31 +0100

    Add action text

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@stimulus-components/rails-nested-form", to: "https://ga.jspm.io/npm:@stimulus-components/rails-nested-form@5.0.0/dist/stimulus-rails-nested-form.mjs"
 pin "sortablejs", to: "https://ga.jspm.io/npm:sortablejs@1.15.6/modular/sortable.esm.js" # @1.15.6
 pin "@rails/request.js", to: "https://ga.jspm.io/npm:@rails/request.js@0.0.11/src/index.js" # @0.0.11
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
