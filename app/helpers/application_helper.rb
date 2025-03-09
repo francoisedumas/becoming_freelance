e9a2a97d0a9ad46079a4102208cbe93e04c2f354
Francois DUMAS LATTAQUE <dumaslattaque.francois@gmail.com>
2025-03-09 18:31:40 +0100

    Use different SVG for learning blocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  SVGS = {
+    0 => 'material-symbols--book-2-outline.svg',
+    1 => 'material-symbols--school-outline.svg',
+    2 => 'material-symbols--table-lamp-outline-sharp',
+  }
+
+  def svg_by_index(index)
+    SVGS[index]
+  end
 end
